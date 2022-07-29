e3632f7238ceaf2e2794adf96612e70cef28a361
dinhgiabao2011 <dinhbb999@gmail.com>
2022-07-29 15:30:43 +0700

    Add project files.

---
+++ PassAppDev/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PassAppDev.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
