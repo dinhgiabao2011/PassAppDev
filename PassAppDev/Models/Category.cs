8a4c39a2bd0712e18f0a9e626de2b0814f652957
Kha Doan <doanvinhkha@gmail.com>
2022-08-05 10:02:44 +0700

    DisplayImageByBook

--- PassAppDev/Models/Category.cs
+++ PassAppDev/Models/Category.cs
@@ -8,18 +8,18 @@ namespace PassAppDev.Models
 {
     public class Category
     {
-        [Key]
-        public int Id { get; set; }
-        [Required(ErrorMessage = "Name cannot be null ...")]
-        [StringLength(255)]
-        public string Name { get; set; }
-        public string OldName { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.Now;
-        public CategoryStatus Status { get; set; } = CategoryStatus.Pending;
+     [Key]
+     public int Id { get; set; }
+     [Required(ErrorMessage = "Name cannot be null ...")]
+     [StringLength(255)]
+     public string Name { get; set; }
+     public string OldName { get; set; }
+     public DateTime CreatedAt { get; set; } = DateTime.Now;
+     public CategoryStatus Status { get; set; } = CategoryStatus.Pending;
 
-        public List<Book> Books { get; set; }
+     public List<Book> Books { get; set; }
 
-    public string ApplicationUserId { get; set; }
-    public ApplicationUser ApplicationUser { get; set; }
+     public string ApplicationUserId { get; set; }
+     public ApplicationUser ApplicationUser { get; set; }
   }
 }
---
+++ PassAppDev/ViewModels/BookViewModel.cs
@@ -0,0 +1,23 @@
+﻿using PassAppDev.Models;
+
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace PassAppDev.ViewModels
+{
+	public class BookViewModel
+	{
+
+    public int Id { get; set; } 
+
+    public string Title { get; set; }
+
+    public string Author { get; set; }
+
+    public float Price { get; set; }
+
+    public Category Category { get; set; }
+
+    public string ImageUrl { get; set; }
+  }
+}
